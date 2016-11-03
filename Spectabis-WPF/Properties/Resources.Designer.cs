d2b398179c2c3f308b2e569e31a95c3ba3dc8b47
Valters <infinity1188@yahoo.com>
2016-11-03 17:58:35 +0200

    - Added boxArt placeholder

--- Spectabis-WPF/Properties/Resources.Designer.cs
+++ Spectabis-WPF/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace Spectabis_WPF.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap art {
+            get {
+                object obj = ResourceManager.GetObject("art", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to .
         /// </summary>
         internal static string String1 {
