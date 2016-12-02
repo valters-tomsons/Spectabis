a1f326620c86ff22a657e01e9dce00cfa08a7d8f
Valters <infinity1188@yahoo.com>
2016-12-02 15:19:39 +0200

    Theme Manager something something

--- Spectabis-WPF/Themes.xaml.cs
+++ Spectabis-WPF/Themes.xaml.cs
@@ -1,13 +1,9 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using MahApps;
-using MaterialDesignThemes;
-using MahApps.Metro;
-using MahApps.Metro.Controls;
 using MaterialDesignThemes.Wpf;
 using System.Collections.Generic;
 using MaterialDesignColors;
-using System.Diagnostics;
+using MahApps;
 
 namespace Spectabis_WPF
 {
@@ -15,20 +11,35 @@ namespace Spectabis_WPF
     /// Interaction logic for Themes.xaml
     /// </summary>
     public partial class Themes : Page
-    { 
+    {
+        //list of all swatches
+        public IEnumerable<Swatch> Swatches { get; }
+
         public Themes()
         {
             InitializeComponent();
+
+            //make a list of all available swatches
+            Swatches = new SwatchesProvider().Swatches;
         }
 
+        //Set Primary color method
         private void SetPrimary(Swatch swatch)
         {
             new PaletteHelper().ReplacePrimaryColor(swatch);
+            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void button_Click(object sender, RoutedEventArgs e)
         {
-            //SetPrimary("BlueGrey700Primary");
+            
+            foreach (Swatch swatch in Swatches)
+            {
+                SetPrimary(swatch);
+                break;
+            }
+            //SetPrimary();
         }
     }
 }
