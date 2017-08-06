458e5ca0f399eaf4eac8c8b75508de6602688220
Faith <infinity1188@yahoo.com>
2017-08-06 05:17:15 +0300

    Add BigPicture project

---
+++ BigPicture/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Configuration;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using BigPicture.Model;
+using BigPicture.Domain;
+
+namespace BigPicture
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DiscoverGames dg = new DiscoverGames();
+            List<GameProfile> games = dg.AllGames();
+
+            Console.WriteLine(SpectabisSettings.EmuDir);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ BigPicture/Model/GameProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace BigPicture.Model
+{
+    public class GameProfile
+    {
+        public string Title { get; set; }
+        public BitmapImage Image { get; set; }
+        public string Platform { get; set; }
+        public string ImageLink { get; set; }
+        public string ProfilePath { get; set; }
+        public string SpectabisIniPath { get; set; }
+    }
+}
