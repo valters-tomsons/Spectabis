9f863a02d9273e0d11cda8cc3d8e49c0c1b14f89
MCatherineE <lavontaebrown@gmail.com>
2018-11-22 23:23:33 -0500

    Added Error Logging, minor bug fix
    Added error logging output to file capability
    Added exception handling for Audio, Video and Input plugin buttons
    Added snackbar on Main Window to display errors
    Fixed issue with PCSX2 directory in settings window not validating correctly

---
+++ Spectabis-WPF/Domain/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectabis_WPF.Domain
+{
+    public static class Logger
+    {
+        public static bool OutputToConsole { get; set; }
+        public static bool OutputToSnackbar { get; set; }
+        public static string LogfilePath { get; set; } = AppDomain.CurrentDomain.BaseDirectory + @"\resources\logs\";
+
+        public static void Init(bool toConsole = true) => OutputToConsole = toConsole;
+
+        public static void Log(params string[] message)
+        {
+            string logfile = LogfilePath + $"{DateTime.Now.ToString("MMddyyyy")}_logfile.txt";
+
+            Directory.CreateDirectory(LogfilePath);
+
+            if (OutputToConsole)
+                Console.WriteLine(message);
+
+            else
+                File.AppendAllLines(logfile, message.Select(x => $"{DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")}:\t {x}"));
+        }
+
+        public static void LogException(Exception e, string message = "")
+        {
+            Log(message,
+                "Exception:\t" + e.Message,
+                "Inner:\t" + e.InnerException?.Message ?? "--",
+                "Stack:\t" + e.StackTrace);
+        }
+    }
+}
