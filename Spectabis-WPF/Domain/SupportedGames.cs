b51850fc191414d8b1dc737af810cbaed2dc086c
FaithLV <valters.tomsons@protonmail.com>
2021-04-15 00:42:44 +0300

    Add .CHD support

--- Spectabis-WPF/Domain/SupportedGames.cs
+++ Spectabis-WPF/Domain/SupportedGames.cs
@@ -6,8 +6,8 @@ namespace Spectabis_WPF.Domain
     {
         //File types that Spectabis supports
         public static List<string> GameFiles = new List<string>() {
-            "iso", "bin", "cso", "gz",
-            "ISO", "BIN", "CSO", "GZ"
+            "iso", "bin", "cso", "gz", "chd",
+            "ISO", "BIN", "CSO", "GZ", "CHD"
 
         };
 
